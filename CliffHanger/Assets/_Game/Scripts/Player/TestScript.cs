77aca7469c29b804e25370104413f948409f1265
tad314 <albin@amneklev.com>
2021-08-12 18:24:07 +0200

    Jag testar

---
+++ CliffHanger/Assets/_Game/Scripts/Player/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets._Game.Scripts.Player
+{
+    public class TestScript : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
